c6af393cd7693348be0e1aea905907bcb9e9fae1
unknown <kd241286zjv@gmail.com>
2019-10-28 09:11:35 +0300

    fixes

--- js/main.js
+++ js/main.js
@@ -10,9 +10,15 @@ $(document).ready(function() {
     });
 
     var portfolioSwiper = new Swiper('.portfolioSection .swiper-container', {
-        navigation: {
-          nextEl: '.portfolioSection .swiper-button-next',
-          prevEl: '.portfolioSection .swiper-button-prev',
-        },
-      });
+      navigation: {
+        nextEl: '.portfolioSection .swiper-button-next',
+        prevEl: '.portfolioSection .swiper-button-prev',
+      },
+    });
+
+    $('a.js-anchor').click(function(){
+      var _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+    });
 })
